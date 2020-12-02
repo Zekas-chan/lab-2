595606c5a1ce213e1ca0079d8a9298b6bbe34d21
Zekas-chan <philar-9@student.ltu.se>
2020-12-02 15:55:36 +0100

    WIP-commit

--- lab2/src/Lab2.java
+++ lab2/src/Lab2.java
@@ -1,3 +1,5 @@
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.Random;
 
 /**
@@ -7,9 +9,9 @@ import java.util.Random;
  */
 public class Lab2 {
 	//levers
-	int arraySizeK = 5;
-	boolean useRandomSeed = true;
-	int fixedSeed = 0;
+	int arraySizeK = 4;
+	boolean useRandomSeed = false;
+	int fixedSeed = 55;
 	
 	//useful globals
 	Random r;
@@ -23,16 +25,20 @@ public class Lab2 {
 	}
 
 	/*
-	 * Tillåter i nuläget identiska element: FIXA!
+	 * 
 	 */
 	int[] generateDataSet() {
 		int x = 13;
 		int y = 42;
 		int z = 69;
 		
+		int nextInsertion;
+		
 		boolean xNotPlaced = true;
 		boolean yNotPlaced = true;
 		boolean zNotPlaced = true;
+		
+		LinkedList<Integer> selectedIntegers = new LinkedList<Integer>();
 		
 		
 		int[] intArr = new int[(int) (3 * Math.pow(2, arraySizeK-1))];
@@ -64,7 +70,11 @@ public class Lab2 {
 			if(intArr[i] != 0) {
 				continue;
 			}else {
-				intArr[i] = 70 + r.nextInt(4000);
+				do {
+				nextInsertion = 70 + r.nextInt(40000);
+				}while(selectedIntegers.contains(nextInsertion));
+				intArr[i] = nextInsertion;
+				selectedIntegers.add(nextInsertion);
 			}
 			
 		}
@@ -80,23 +90,20 @@ public class Lab2 {
 //	=== Part 1-1 algorithms ===
 	
 	int[] incrementalAlg(int[] input) {
-		int x = Integer.MAX_VALUE;
-		int y = Integer.MAX_VALUE;
-		int z = Integer.MAX_VALUE;
+		int x = max_value(input);
+		int y = x;
+		int z = x;
 		
 		for(int i : input) {
-			incrementalAlgCount++;
-			if(i < x) {
-				incrementalAlgCount++;
+//			incrementalAlgCount++;
+			if(boolCounter(i, x)) {
 				z = y;
 				y = x;
 				x = i;
-			}else if(i < y) {
-				incrementalAlgCount++;
+			}else if((boolCounter(i, y))) {
 				z = y;
 				y = i;
-			}else if(i < z) {
-				incrementalAlgCount++;
+			}else if(boolCounter(i, z)) {
 				z = i;
 			}
 		}
@@ -104,30 +111,70 @@ public class Lab2 {
 		return new int[] {x, y, z};
 	}
 	
+	/**
+	 * Hjälpmetod
+	 * Räknar upp antalet jämförelser för inkrementella algoritmen
+	 * @param a Tal som ska jämföras
+	 * @param b Tal som ska jämföras
+	 * @return true om a < b, false annars
+	 */
+	boolean boolCounter(int a, int b) {
+		incrementalAlgCount++;
+		if(a < b) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	int[] incrementalAlgv2(int[] input) {
+		int x = max_value(input);
+		int y = x;
+		int z = x;
+		
+		for(int i : input) {
+//			incrementalAlgCount++;
+			if(boolCounter(i, z)) {
+				if(boolCounter(i, x)) {
+					z = y;
+					y = x;
+					x = i;
+				}else if(boolCounter(i, y)) {
+					z = y;
+					y = i;
+				}else {
+					z = i;
+				}
+			}
+		}
+		return new int[] {x, y, z};
+	}
+	
 	public int[] divideAndConquer(int[] arr) {
-
+        int[] finalArr = divideConquer(arr);
+        divideAndConquerAlgCount++;
+        return finalArr;
+        //return new int[]{finalArr[0], finalArr[1], finalArr[2]};
+    }
+    private int[] divideConquer(int[] arr) {
         if(arr.length == 3) {
-            return arr;
+            divideAndConquerAlgCount++;
+            return sort(arr);
         }
         else {
             int[] arr1 = new int[arr.length/2];
             int[] arr2 = new int[arr.length/2];
-            for(int i = 0; i < arr1.length; i++) {
-                arr1[i] = arr[i];
-                arr2[i] = arr[arr2.length + i];
-            }
-            int[] arr3 = divideAndConquer(sort(arr1));
-            int[] arr4 = divideAndConquer(sort(arr2));
-            int[] finalArr = new int[arr3.length + arr4.length];
-            for(int i = 0; i < finalArr.length; i++) {
-                if(i < arr3.length) {
-                    finalArr[i] = arr3[i];
-                }
-                else {
-                    finalArr[i] = arr4[i - arr4.length];
-                }
-            }
-            return sort(finalArr);
+            System.arraycopy(arr, 0, arr1, 0, arr.length/2);
+            System.arraycopy(arr, arr.length/2, arr2, 0, arr.length/2);
+            arr1 = divideConquer(arr1);
+            divideAndConquerAlgCount++;
+            arr2 = divideConquer(arr2);
+            divideAndConquerAlgCount++;
+            int[] finalArr = new int[arr1.length + arr2.length];
+            System.arraycopy(arr1, 0, finalArr, 0, arr1.length);
+            System.arraycopy(arr2, 0, finalArr, arr1.length, arr2.length);
+            divideAndConquerAlgCount++;
+            return finalArr;
         }
     }
 
@@ -137,12 +184,12 @@ public class Lab2 {
 
         int output[] = new int[n]; 
 
-        int count[] = new int[4070];
+        int count[] = new int[max+1];
 
         for (int i = 0; i < n; ++i) 
             ++count[arr[i]]; 
 
-        for (int i = 1; i <= 4070-1; ++i) 
+        for (int i = 1; i <= max; ++i) 
             count[i] += count[i - 1]; 
  
         for (int i = n - 1; i >= 0; i--) { 
@@ -152,12 +199,18 @@ public class Lab2 {
         return output;
     }
     
+    /**
+     * Hittar det största heltalet i en array och returnerar det
+     * @param nums Array med heltal
+     * @return Det största heltalet i arrayen
+     */
     int max_value(int[] nums) 
     {
         int max_value = 0;
         for (int i = 0; i < nums.length; i++)
             if (nums[i] > max_value)
                 max_value = nums[i];
+//        System.out.println("Max value found: "+max_value);
         return max_value;
     } 
 	
@@ -167,9 +220,32 @@ public class Lab2 {
 	public static void main(String[] args) {
 		Lab2 asdf = new Lab2();
 		
+		
+		System.out.println("\nTesting Incremental Algorithm: ");
 		asdf.testIncrementalAlg();
 		System.out.print("\n");
+		
+		System.out.println("\nTesting Incremental Algorithm v2: ");
+		asdf.testIncrementalAlgv2();
+		System.out.print("\n");
+		
+		System.out.println("\nTesting Divide and Conquer Algorithm: ");
 		asdf.testDivideAndConquerAlg();
+		System.out.print("\n");
+	}
+	
+	void testIncrementalAlgv2() {
+		int[] test = generateDataSet();
+		
+		int[] result = incrementalAlgv2(test);
+		
+		for(int i : result) {
+			System.out.print(i+", ");
+		}
+		System.out.println("\nNumber of elements: "+test.length);
+		System.out.println("Executed in "+incrementalAlgCount);
+		
+		incrementalAlgCount = 0;
 		
 	}
 	
@@ -184,6 +260,7 @@ public class Lab2 {
 		System.out.println("\nNumber of elements: "+test.length);
 		System.out.println("Executed in "+incrementalAlgCount);
 		
+		incrementalAlgCount = 0;
 	}
 	
 	void testDivideAndConquerAlg() {
